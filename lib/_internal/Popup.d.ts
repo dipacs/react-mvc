b19f8a65a12ac4ce7c15b84a48e830443115f825
David Ipacs <david.ipacs@gmail.com>
2019-01-29 10:15:31 +0100

    Popup fix

--- lib/_internal/Popup.d.ts
+++ lib/_internal/Popup.d.ts
@@ -1,10 +1,9 @@
-/// <reference types="react" />
 import * as React from 'react';
 export declare class Popup extends React.Component<PopupProps, any> {
     constructor(props: PopupProps);
     onFadeClicked(): void;
     render(): JSX.Element;
-    private createFadeStyle();
+    private createFadeStyle;
 }
 export declare class PopupProps {
     fadeColor?: string;
